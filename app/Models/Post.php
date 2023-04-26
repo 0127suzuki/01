576d0448f5fbb7d8e1ab35fbee0838ec0443a2f3
0127suzuki <uraohara@gmail.com>
2023-04-27 00:06:30 +0900

    8-4-3

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -10,6 +10,11 @@ class Post extends Model
 {
     use HasFactory;
     
+    protected $fillable = [
+         'title',
+         'body'
+        ];
+    
     public function getByLimit(int $limit_count = 10)
    {
     return $this->orderBy('updated_at', 'DESC')->paginate($limit_count);
