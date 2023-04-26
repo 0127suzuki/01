79cf4df93736f35e14c9cefcc623e2ec75cba77a
0127suzuki <uraohara@gmail.com>
2023-04-27 00:57:58 +0900

    8-4-5

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,14 +3,14 @@
 namespace App\Http\Controllers;
 
 use App\Models\Post;
-use Illuminate\Http\Request;
+use App\Http\Requests\PostRequest;
 
 
 class PostController extends Controller
 {
     public function index(Post $post)
     {
-        return view('posts/index')->with(['posts' => $post->getByLimit(1)]);
+        return view('posts/index')->with(['posts' => $post->getByLimit()]);
     }
     //
  /**
@@ -29,7 +29,7 @@ class PostController extends Controller
         return view('posts/create');
     }
     
-    public function store(Request $request, Post $post)
+    public function store(Post $post, PostRequest $request)
     {
         $input = $request['post'];
         $post->fill($input)->save();
