dfca29b702e182e024f56bad45fcc13cd14cf956
0127suzuki <uraohara@gmail.com>
2023-04-28 01:27:52 +0900

    適切なコミットコメント

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -10,6 +10,7 @@ class Post extends Model
 {
     use HasFactory;
     
+
     public function getByLimit(int $limit_count = 10)
    {
     return $this->orderBy('updated_at', 'DESC')->paginate($limit_count);
